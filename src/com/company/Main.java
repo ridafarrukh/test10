a4f239948c4b6958e9506a70d6cc7955cab7077e
ridafarrukh <ridafarrukh92@gmail.com>
2020-10-25 00:44:34 +0200

    new file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hej");
+        System.out.println("hejdå");
+        System.out.println("välkomman");
     }
 }
